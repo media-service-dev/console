d9b20f3319787f14fe0f379207485d7cbc23a9fc
chapterjason <jason@sourecode.dev>
2021-02-04 14:04:05 +0100

    Update copyright year

--- src/Input/InputInterface.ts
+++ src/Input/InputInterface.ts
@@ -1,7 +1,7 @@
 /*
  * This file is part of the @mscs/console package.
  *
- * Copyright (c) 2020 media-service consulting & solutions GmbH
+ * Copyright (c) 2021 media-service consulting & solutions GmbH
  *
  * For the full copyright and license information, please view the LICENSE
  * File that was distributed with this source code.
